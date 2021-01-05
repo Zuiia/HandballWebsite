05b9dbd1a6477431ae8feb01632671dbfe4b9d48
jgonka <jonas.gonka@googlemail.com>
2021-01-05 17:51:45 +0100

    feat: Navbar v1 added

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
@@ -10,6 +11,17 @@ export class NavbarComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+      $('.dropright button').on('click', (e) => {
+        e.stopPropagation();
+        e.preventDefault();
+
+        if (!$(this).next('div').hasClass('show')) {
+          $(this).next('div').addClass('show');
+        } else {
+          $(this).next('div').removeClass('show');
+        }
+
+      });
   }
 
 }
